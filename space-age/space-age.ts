ab8116f906baf413e3f4c73be635ec1a2befb92c
PhilipeeX <dev.philipe@outlook.com>
2024-10-20 16:29:00 -0300

    refactor: Creates function to handle age on earth

--- space-age/space-age.ts
+++ space-age/space-age.ts
@@ -1,19 +1,24 @@
 const orbitalPeriods: { [key: string]: number } = {
       mercury: 0.2408467,
-      venus: 0.161519726,
+      venus: 0.61519726,
       earth: 1,
-      mars: 3,
-      jupiter: 4,
-      saturn: 5,
-      uranus: 6,
-      neptune: 7,
+      mars: 1.8808158,
+      jupiter: 11.862615,
+      saturn: 29.447498,
+      uranus: 84.016846,
+      neptune: 164.79132,
 };
 
 export function age(planet: string, seconds: number): number {
-  let inEarth = 31557600;
-  let earth = seconds / inEarth;
   let result: number = 0;
-  result = earth / orbitalPeriods[planet];
+  result = Number((calculatesAgeOnEarth(seconds) / orbitalPeriods[planet]).toFixed(2));
 
-  return Number(result.toFixed(2));
+  return result;
 };
+
+export function calculatesAgeOnEarth(seconds: number): number {
+  let ageOnEarth: number = 31557600;
+  ageOnEarth = seconds / ageOnEarth;
+  return ageOnEarth;
+};
+
