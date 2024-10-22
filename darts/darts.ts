78ea6ad7a1a2531d743e55d4cf391bf5290dd881
PhilipeeX <dev.philipe@outlook.com>
2024-10-22 20:46:11 -0300

    passing all the tests

--- darts/darts.ts
+++ darts/darts.ts
@@ -1,13 +1,13 @@
-let points: number;
+let points: number = 0;
 
 export function score(x: number, y: number): number {
 
   if (x ** 2 + y ** 2 <= 1) {
-    points += 10;
+    points = 10;
   } else if (x ** 2 + y ** 2 <= 25) {
-    points += 5;
+    points = 5;
   }else if (x ** 2 + y ** 2 <= 100) {
-    points += 1;
+    points = 1;
   }else {
     points = 0;
   }
